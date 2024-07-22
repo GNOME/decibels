85c9bd9aef35e23b31590532f2885b5d07bc63dc
Angelo Verlain <geoangercola@gmail.com>
2024-07-22 18:15:13 +0200

    feat: add new window action

--- src/application.ts
+++ src/application.ts
@@ -3,11 +3,9 @@ import Gio from "gi://Gio";
 import GObject from "gi://GObject";
 import Gtk from "gi://Gtk?version=4.0";
 
-import { Window } from "./window.js";
+import { AddActionEntries, Window } from "./window.js";
 
 export class Application extends Adw.Application {
-  private window?: Window;
-
   static {
     GObject.registerClass(this);
   }
@@ -19,14 +17,23 @@ export class Application extends Adw.Application {
       flags: Gio.ApplicationFlags.HANDLES_OPEN,
     });
 
-    const quit_action = new Gio.SimpleAction({ name: "quit" });
-    quit_action.connect("activate", () => {
-      this.quit();
-    });
+    (this.add_action_entries as AddActionEntries)([
+      {
+        name: "new-window",
+        activate: () => {
+          this.present_new_window();
+        },
+      },
+      {
+        name: "quit",
+        activate: () => {
+          this.quit();
+        },
+      },
+    ]);
 
-    this.add_action(quit_action);
+    this.set_accels_for_action("app.new-window", ["<Control>n"]);
     this.set_accels_for_action("app.quit", ["<Control>q"]);
-
     this.set_accels_for_action("win.open-file", ["<Control>o"]);
 
     const show_about_action = new Gio.SimpleAction({ name: "about" });
@@ -56,25 +63,21 @@ export class Application extends Adw.Application {
     aboutDialog.present(this.get_active_window());
   }
 
-  private present_main_window(): void {
-    if (!this.window) {
-      this.window = new Window({ application: this });
-      if (pkg.name.endsWith("Devel")) this.window.add_css_class("devel");
-    }
-
-    this.window.present();
+  private present_new_window() {
+    const window = new Window({ application: this });
+    if (pkg.name.endsWith("Devel")) window.add_css_class("devel");
+    window.present();
+    return window;
   }
 
   vfunc_activate(): void {
-    this.present_main_window();
+    this.present_new_window();
   }
 
   vfunc_open(files: Gio.FilePrototype[]): void {
-    this.present_main_window();
+    const window = this.get_active_window() ?? this.present_new_window();
 
-    const window = this.get_active_window();
-
-    if (window && window instanceof Window && files.length > 0) {
+    if (window instanceof Window && files.length > 0) {
       void window.load_file(files[0]);
     }
   }
